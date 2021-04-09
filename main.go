9b5b68c5245c3d2f62c4c34d7a8d66aa2bb8819b
raihaninfo <raihanmahmudi35@gmail.com>
2021-04-09 21:15:54 +0600

    unused code remove

--- main.go
+++ main.go
@@ -6,16 +6,17 @@ import (
 	"github.com/jung-kurt/gofpdf"
 )
 
+const (
+	bannerHt = 94.0
+	xIndent  = 40.0
+)
+
 func main() {
 	pdf := gofpdf.New(gofpdf.OrientationPortrait, gofpdf.UnitPoint, gofpdf.PageSizeLetter, "")
 	w, h := pdf.GetPageSize()
 	fmt.Printf("width=%v, height=%v\n", w, h)
 	pdf.AddPage()
 
-	const (
-		bannerHt = 94.0
-		xIndent  = 40.0
-	)
 	pdf.SetFillColor(103, 60, 79)
 	pdf.Polygon([]gofpdf.PointType{
 		{0, 0},
@@ -30,10 +31,19 @@ func main() {
 		{w, h},
 	}, "F")
 
+	// INVOICE
 	pdf.SetFont("Arial", "B", 40)
 	pdf.SetTextColor(255, 255, 255)
 	_, lineHt := pdf.GetFontSize()
 	pdf.Text(xIndent, bannerHt-(bannerHt/2.0)+lineHt/2.9, "INVOICE")
+
+	// Address & company info
+
+	// pdf.SetFont("arial", "", 12)
+	// pdf.SetTextColor(255, 255, 255)
+	// _, lineHt = pdf.GetFontSize()
+
+	// pdf.MultiCell(124.0, lineHt *1.5, "01853566901\n raihan@gmail.com\nraihaninfo.com ",)
 
 	// Grid
 	drawGrid(pdf)
@@ -50,12 +60,17 @@ func drawGrid(pdf *gofpdf.Fpdf) {
 	pdf.SetTextColor(80, 80, 80)
 	pdf.SetDrawColor(200, 200, 200)
 	for x := 0.0; x < w; x = x + (w / 20.0) {
-
+		pdf.SetTextColor(200, 200, 200)
 		pdf.Line(x, 0, x, h)
 		_, lineHt := pdf.GetFontSize()
 		pdf.Text(x, lineHt, fmt.Sprintf("%d", int(x)))
 	}
 	for y := 0.0; y < h; y = y + (w / 20.0) {
+		if y < bannerHt {
+			pdf.SetTextColor(200, 200, 200)
+		} else {
+			pdf.SetTextColor(80, 80, 80)
+		}
 		pdf.Line(0, y, w, y)
 		pdf.Text(0, y, fmt.Sprintf("%d", int(y)))
 	}
